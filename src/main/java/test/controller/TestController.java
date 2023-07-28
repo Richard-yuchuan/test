73d83ffc841f723bab53a29544a635fd361a9665
Richard-yuchuan <xx584620@gmail.com>
2023-07-28 14:47:18 +0800

    Project initialize

---
+++ src/main/java/test/controller/TestController.java
@@ -0,0 +1,22 @@
+package test.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		var xxx = req.getParameter("para");
+		req.setAttribute("nickname", xxx);
+		req.getRequestDispatcher("/index.jsp").forward(req, res);
+	}
+
+}
